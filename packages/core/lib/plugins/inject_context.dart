8d5110bcf73acdb31c915846dd917f40d4bbce62
lucky <lucky@weareclario.com>
2023-11-03 09:44:35 +0100

    Fixed filling IP on segment server side.

--- packages/core/lib/plugins/inject_context.dart
+++ packages/core/lib/plugins/inject_context.dart
@@ -2,6 +2,7 @@ import 'package:analytics/analytics.dart';
 import 'package:analytics/event.dart';
 import 'package:analytics/plugin.dart';
 import 'package:uuid/uuid.dart';
+import 'dart:io' show Platform;
 
 class InjectContext extends PlatformPlugin {
   InjectContext() : super(PluginType.before);
@@ -13,7 +14,9 @@ class InjectContext extends PlatformPlugin {
     // We need to get the Context in a concurrency safe mode to permit changes to make it in before we retrieve it
     final context = await analytics!.state.context.state;
     context!.instanceId = instanceId;
-    context.library = ContextLibrary("analytics-flutter", Analytics.version());
+    final libraryName =
+        Platform.isAndroid ? "analytics-android" : "analytics-ios";
+    context.library = ContextLibrary(libraryName, Analytics.version());
     event.context = context;
     return event;
   }
